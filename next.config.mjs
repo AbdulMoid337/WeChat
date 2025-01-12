85103ab9f93bbb546cd8b0f1a03304e282a932a5
Abdul Moid <abdulmoid337@gmail.com>
2025-01-12 20:59:06 +0530

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,8 @@
 // next.config.js
-const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    domains: ['upload.wikimedia.org'], // Add external domains for images
-  },
-  publicRuntimeConfig: {
-    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
-    STREAM_KEY: process.env.STREAM_KEY,
-  },
-  serverRuntimeConfig: {
-    CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
-    STREAM_SECRET_KEY: process.env.STREAM_SECRET_KEY,
-  },
-};
-
+export default {
+    reactStrictMode: true,
+    images: {
+      domains: ['upload.wikimedia.org'], // Add external domains for images
+    },
+  };
+  
